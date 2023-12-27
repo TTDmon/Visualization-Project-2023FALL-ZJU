8d2901f270c313ff8e3f860d84ee4009b16d5ad7
TTDmon <mingryin@gmail.com>
2023-12-27 11:26:18 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import {useState,useRef} from "react";
+import {Canvas, useFrame} from "@react-three/fiber";
 import './App.css';
+
+function Cube(props) {
+  // Use useRef hook to access the mesh element
+  const mesh = useRef();
+
+  // State values for hover
+  const [hovered, setHover] = useState(false);
+
+  //Basic animation to rotate our cube using animation frame
+  useFrame(() => (mesh.current.rotation.x += 0.01));
+
+  // Jsx to render our 3d cube. Our cube will have height
+  // width and depth equal 2 units.
+  // You also need a material so that you can add color
+  // and show shadows. We are using the standard
+  // material <<meshStandardMaterial />
+
+  return (
+    <mesh
+      ref={mesh}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10,10,10]} />
+      <Cube />
+    </Canvas>
   );
 }
 
